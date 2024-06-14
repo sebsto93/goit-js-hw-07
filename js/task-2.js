f9a7114a5abf2c4e07b004a6b70da0489aedc75b
sebsto93 <s.stobiecki93@gmail.com>
2024-06-14 15:27:22 +0200

    task-2 done

--- js/task-2.js
+++ js/task-2.js
@@ -24,3 +24,11 @@ const images = [
     alt: "Lighthouse Coast Sea",
   }
 ];
+
+const gallery = document.querySelector(".gallery");
+const galleryItems = images.map(image => 
+  `<li class="gallery-item">
+  <img src="${image.url}" alt="${image.alt}">
+  </li>`).join("");
+
+  gallery.insertAdjacentHTML("beforeend", galleryItems);
